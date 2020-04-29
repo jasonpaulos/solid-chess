492836d02f1831ae34488b59e5c0f69a68ee5a3a
Jason Paulos <jasonpaulos@users.noreply.github.com>
2020-04-29 13:51:29 -0400

    Add navigation

--- index.android.js
+++ index.android.js
@@ -1,5 +1,1 @@
-import { AppRegistry } from 'react-native';
-import App from './mobile-app/App';
-import { name as appName } from './mobile-app/app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+import './mobile-app';
