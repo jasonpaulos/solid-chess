0518f1e6a9811694ed192a8c4b131b5b9c6327d8
Jason Paulos <jasonpaulos@users.noreply.github.com>
2020-04-29 11:23:29 -0400

    Init react native app

---
+++ index.android.js
@@ -0,0 +1,5 @@
+import { AppRegistry } from 'react-native';
+import App from './mobile-app/App';
+import { name as appName } from './mobile-app/app.json';
+
+AppRegistry.registerComponent(appName, () => App);
